2f20513389aef0efbe71490bf29db98994b98e54
unknown <ashley.elowsky123@gmail.com>
2019-09-07 16:23:16 -0700

    working on assignment questions

---
+++ index.js
@@ -0,0 +1,24 @@
+//Step 1 Convert to CamelCase notation
+// var someMonth;
+
+//function to call the current month 
+// function theMonth() {
+//     var today = new Date();
+//     var mm = today.getMonth()+1;
+//     console.log(mm);
+// }
+
+// theMonth();
+
+//currentMonth
+//var summerMonth = ["June", "July", "August"];
+//function myLibrary() {};
+
+//Step 2 examples:numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression
+//numeric literal expression = 117, 00002, 0x1123;
+//string literal expression = "cat", "1234", "bugs bunny"
+//boolean literal expression = true, false
+//null literal expression = null
+
+//Step 3 Give me two examples of complex/variable expressions
+//
